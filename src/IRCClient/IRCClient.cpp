fc510e130383d986af48811efe37e5a7854a5055
Ellie <ellie1337@gmail.com>
2013-11-01 13:33:26 -0700

    Fix up the IRCClient constructor

--- src/IRCClient/IRCClient.cpp
+++ src/IRCClient/IRCClient.cpp
@@ -1,6 +1,7 @@
 #include "preCompiled.h"
 
-IRCClient::IRCClient(const std::string& hostname, unsigned int port) : Socket(hostname, port)
+IRCClient::IRCClient(const std::string& hostname, unsigned int port, const std::string& user, const std::string& nick, const std::string& aNick)
+	: Socket(hostname, port), joinUser(user), joinNickname(nick), joinAltNickname(aNick)
 {
 }
 
--- src/IRCClient/IRCClient.h
+++ src/IRCClient/IRCClient.h
@@ -2,7 +2,14 @@
 
 class IRCClient : public Socket
 {
+private:
+	std::vector<std::string> channels;
+	std::string activeNickname;
+	std::string joinUser;
+	std::string joinNickname;
+	std::string joinAltNickname;
+
 public:
-	IRCClient(const std::string& hostname, unsigned int port = 6667);
+	IRCClient(const std::string& hostname, unsigned int port, const std::string& user, const std::string& nick, const std::string& aNick);
 	~IRCClient();
 };
