3e96d9e34685d845715830438a8ad262c6dcde5a
Ellie <ellie1337@gmail.com>
2013-11-01 10:27:02 -0700

    libeay32.dll isn't needed, only ssleay32.dll

--- win/CopyDLLs.bat
+++ win/CopyDLLs.bat
@@ -4,13 +4,11 @@ goto EOF
 
 :x86
 copy "..\dep\MariaDB\win32\lib\libmariadb.dll" "%~2" /Y
-copy "..\dep\OpenSSL\win32\libeay32.dll" "%~2" /Y
 copy "..\dep\OpenSSL\win32\ssleay32.dll" "%~2" /Y
 goto EOF
 
 :x64
 copy "..\dep\MariaDB\win64\lib\libmariadb.dll" "%~2" /Y
-copy "..\dep\OpenSSL\win64\libeay32.dll" "%~2" /Y
 copy "..\dep\OpenSSL\win64\ssleay32.dll" "%~2" /Y
 goto EOF
 
