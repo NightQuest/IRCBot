c9a38a967540629884fdb499716bedb22b8e866c
Ellie <ellie1337@gmail.com>
2013-12-08 17:53:16 -0800

    Added kelvin conversion

--- src/Scripts/TemperatureConversion.cpp
+++ src/Scripts/TemperatureConversion.cpp
@@ -8,11 +8,26 @@ public:
 	std::vector<ChatCommand> getCommands() override
 	{
 		ChatCommand commands[] = {
-			{ "f2c", "Adds perks to a specified account", handleFahrenheitToCelcius, ACCESS_NONE, vector<ChatCommand>() },
-			{ "c2f", "Removes perks from a specified account", handleCelciusToFahrenheit, ACCESS_NONE, vector<ChatCommand>() }
+			{ "f2k", "Converts from Fahrenheit to Kelvin", handleFahrenheitToKelvin, ACCESS_NONE, vector<ChatCommand>() },
+			{ "f2c", "Converts from Fahrenheit to Celcius", handleFahrenheitToCelcius, ACCESS_NONE, vector<ChatCommand>() },
+
+			{ "c2k", "Converts from Celcius to Kelvin", handleCelciusToKelvin, ACCESS_NONE, vector<ChatCommand>() },
+			{ "c2f", "Converts from Celcius to Fahrenheit", handleCelciusToFahrenheit, ACCESS_NONE, vector<ChatCommand>() },
+
+			{ "k2f", "Converts from Kelvin to Fahrenheit", handleKelvinToFahrenheit, ACCESS_NONE, vector<ChatCommand>() },
+			{ "k2c", "Converts from Kelvin to Celcius", handleKelvinToCelcius, ACCESS_NONE, vector<ChatCommand>() },
 		};
 
 		return vector<ChatCommand>(begin(commands), end(commands));
+	}
+
+	static void handleFahrenheitToKelvin(const std::shared_ptr<User>& user, const std::string& target, const std::string& arguments)
+	{
+		string messageTarget = (target == sSock->getNickname()) ? user->getNickname() : target;
+		stringstream ss;
+		ss << fixed << setprecision(2) << (ceilf((((stof(arguments) - 32.0f) / 1.8f) + 273.15f) * 100) / 100);
+
+		sSock->sendMessage(messageTarget, ss.str() + " K");
 	}
 
 	static void handleFahrenheitToCelcius(const std::shared_ptr<User>& user, const std::string& target, const std::string& arguments)
@@ -24,6 +39,15 @@ public:
 		sSock->sendMessage(messageTarget, ss.str() + " °C");
 	}
 
+	static void handleCelciusToKelvin(const std::shared_ptr<User>& user, const std::string& target, const std::string& arguments)
+	{
+		string messageTarget = (target == sSock->getNickname()) ? user->getNickname() : target;
+		stringstream ss;
+		ss << fixed << setprecision(2) << (ceilf((stof(arguments) + 273.15f) * 100) / 100);
+
+		sSock->sendMessage(messageTarget, ss.str() + " K");
+	}
+
 	static void handleCelciusToFahrenheit(const std::shared_ptr<User>& user, const std::string& target, const std::string& arguments)
 	{
 		string messageTarget = (target == sSock->getNickname()) ? user->getNickname() : target;
@@ -32,9 +56,28 @@ public:
 
 		sSock->sendMessage(messageTarget, ss.str() + " °F");
 	}
+
+	static void handleKelvinToFahrenheit(const std::shared_ptr<User>& user, const std::string& target, const std::string& arguments)
+	{
+		string messageTarget = (target == sSock->getNickname()) ? user->getNickname() : target;
+		stringstream ss;
+		ss << fixed << setprecision(2) << (ceilf((((stof(arguments) - 273.15f) * 1.8f) + 32.0f) * 100) / 100);
+
+		sSock->sendMessage(messageTarget, ss.str() + " °F");
+	}
+
+	static void handleKelvinToCelcius(const std::shared_ptr<User>& user, const std::string& target, const std::string& arguments)
+	{
+		string messageTarget = (target == sSock->getNickname()) ? user->getNickname() : target;
+		stringstream ss;
+		ss << fixed << setprecision(2) << (ceilf((stof(arguments) - 273.15f) * 100) / 100);
+
+		sSock->sendMessage(messageTarget, ss.str() + " °C");
+	}
+
 };
 
 void setupTemperatureConversion()
 {
-	new TemperatureConversion;
+	new TemperatureConversion();
 }
