55b8a2f9313cd92390235980d5f7b1f843e8a313
Ellie <ellie1337@gmail.com>
2015-07-13 18:05:38 -0700

    Compile fixes

--- src/MariaDB/MariaDB.h
+++ src/MariaDB/MariaDB.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "preCompiled.h"
 
+typedef unsigned short  ushort;
+typedef unsigned int    uint;
+typedef unsigned __int64 ulonglong;
+typedef __int64	longlong;
+typedef int sigset_t;
+typedef	long off_t;
+typedef __int64 os_off_t;
+#ifdef _WIN64
+typedef ulonglong rf_SetTimer;
+#else
+typedef uint rf_SetTimer;
+#endif
+
 #include <mysql.h>
+
 #include "QueryRowField.h"
 #include "QueryRow.h"
 #include "QueryResult.h"
